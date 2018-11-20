141f8d2a42cf16a8563aed7c8490531e10e861c0
chloe <ryouhei05205005@gmail.com>
2018-11-20 12:44:20 +0900

    いったんリセット

--- explosion.h
+++
@@ -1,9 +0,0 @@
-#ifndef EXPLOSION_H_
-#define EXPLOSION_H_
-
-void Explosion_Initialize(void);
-void Explosion_Update(void);
-void Explosion_Draw(void);
-void Explosion_Create(float x, float y, float z);
-
-#endif // EXPLOSION_H_
--- light.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIGHT_H_
-#define LIGHT_H_
-
-void Light_Initialize(void);
-void Light_SetLight(void);
-
-
-#endif // LIGHT_H_
